14f11614733e40d2b7fe89c71729c55086d9d6d6
Ramesh <ramesh.arva7@gmail.com>
2024-03-10 13:12:14 +0530

    Initial chnages

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='erpsqlapp',
+    version='1.0',
+    author='Ramesh Arvapalli',
+    author_email='ramesh.arva7@email.com',
+    description='A Streamlit application for managing cloud connections.',    
+    long_description_content_type='text/markdown',    
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'streamlit',
+        'requests',
+        'pandas'
+    ],
+     entry_points={
+        'console_scripts': [
+            'erpsqlapp=cloudsql.ERPCloudConsole:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
