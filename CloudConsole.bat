285871ed638eeece76b111cdb4e4d388bc0a0988
Ramesh <ramesh.arva7@gmail.com>
2024-04-17 22:24:46 +0530

    renames file names

--- CloudConsole.bat
+++ CloudConsole.bat
@@ -6,7 +6,7 @@ REM Step 2: Navigate to ERPE2E\tests folder
 cd /d "%~dp0cloudsql"
 
 REM Step 3: Run pyteest -m jobs command
-streamlit run ERPCloudConsole.py
+streamlit run app.py
 
 REM Step 4: Deactivate py_automate venv (optional)
 deactivate
